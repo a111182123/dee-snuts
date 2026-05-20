1fa54a9457fe49fcf712c3c4a7d1d63fdf9bbdc2
a111182123 <a111182123@nkust.edu.tw>
2026-05-20 19:15:39 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/dee-snuts/', 
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
